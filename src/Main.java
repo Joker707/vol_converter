01296003255ff6c2f2cb616ebf1d41b147df3f1d
joker <ser.fyodoroff@yandex.ru>
2022-01-05 21:51:05 +0300

    #3 incorrect input fixed

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,38 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("insert input type ('bls', 'gal' or 'l'): ");
+        System.out.println("insert input type ('bls', 'gal' or 'l') or 'stop' to stop converter: ");
         String inputType = bufferedReader.readLine();
-        System.out.println("insert your value: ");
-        Double value = Double.parseDouble(bufferedReader.readLine());
+        while (true) {
+            if (inputType.equals("stop")) {
+                return;
+            } else if (inputType.equals("bls") || inputType.equals("gal") | inputType.equals("l")) {
+                break;
+            } else {
+                System.out.println("incorrect type, insert 'bls', 'gal', 'l' or 'stop' to stop converter: ");
+                inputType = bufferedReader.readLine();
+            }
+        }
+        Double value = null;
+        do {
+            System.out.println("insert your value: ");
+            String number = bufferedReader.readLine();
+            try {
+                value = Double.parseDouble(number);
+            } catch (NumberFormatException e) {
+                System.out.println("That's not a number");
+            }
+        } while (value == null);
         System.out.println("insert output type ('bls', 'gal' or 'l'):");
         String outputType = bufferedReader.readLine();
+        while (true) {
+            if (outputType.equals("bls") || outputType.equals("gal") | outputType.equals("l")) {
+                break;
+            } else {
+                System.out.println("incorrect type, insert 'bls', 'gal' or  'l' : ");
+                outputType = bufferedReader.readLine();
+            }
+        }
         String fullType = inputType + " -> " + outputType;
         result(fullType, value);
     }
